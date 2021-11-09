8a1d0ff0a83f5d24999f759290f49429ebd7cea6
joelweeweijie <57750835+joelweeweijie@users.noreply.github.com>
2021-11-09 19:08:59 +0800

    [New] Scenario 3 Street Lights toggle with "L"
    Added a script to control the toggle of street lights with "L" key.

---
+++ ICT3104_unity/Assets/Script/Light/Streetlight.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Streetlight : MonoBehaviour
+{
+	Light light;
+
+	// Use this for initialization
+	void Start()
+	{
+		light = GetComponent<Light>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// Toggle light on/off when L key is pressed.
+		if (Input.GetKeyUp(KeyCode.L))
+		{
+			light.enabled = !light.enabled;
+		}
+	}
+}
